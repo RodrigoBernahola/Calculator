aac185f965cb1c05fd406e73e52313d0e48b91d6
rodrigol <rodrigobernahola28@gmail.com>
2024-06-01 01:45:41 -0300

    Se agregara el funcionamiento basico y los botones
    Se puede realizar una sola operacion a la vez faltan implementar los
    botones clear y posibles bugs.

--- script.js
+++ script.js
@@ -11,39 +11,65 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-
     if (b !== 0) {
         return a / b;
     }
-
-    return 'Error division by 0 not allowed';
+    return alert('Error: division by 0 not allowed');
 }
 
 let operandoUno = 0;
 let operandoDos = 0;
 let operador = "";
 
-
 function operate(operator, numberOne, numberTwo) {
-
     switch (operator) {
-
         case "*":
-            multiply(numberOne, numberTwo);
-            break;
-        
+            return multiply(numberOne, numberTwo);
         case "+":
-            add(numberOne, numberTwo);
-            break;
-
+            return add(numberOne, numberTwo);
         case "-":
-            subtract(numberOne, numberTwo);
-            break;
-
+            return subtract(numberOne, numberTwo);
         case "/":
-            divide(numberOne, numberTwo);
-            break;
+            return divide(numberOne, numberTwo);
+        default:
+            return null;
+    }
+}
 
+let paraDisplay = document.querySelector('#displayParagraph');
+let divDigits = document.querySelector('.underDisplay');
+let equalButton = document.querySelector('#equal');
+
+divDigits.addEventListener('click', (evento) => {
+    let string = evento.target.textContent;
+    paraDisplay.textContent += string;
+});
+
+equalButton.addEventListener('click', (evento) => {
+    let cant = paraDisplay.textContent.length;
+    let operadorFound = false;
+    let numbers = "0123456789";
+    let firstNumber = '';
+    let secondNumber = '';
+    let operadorFromOperation = '';
+
+    for (let i = 0; i < cant; i++) {
+        let char = paraDisplay.textContent[i];
+        if (operadorFound) {
+            secondNumber += char;
+        } else {
+            if (numbers.includes(char)) {
+                firstNumber += char;
+            } else {
+                operadorFound = true;
+                operadorFromOperation = char;
+            }
+        }
     }
 
-}
+    firstNumber = parseFloat(firstNumber);
+    secondNumber = parseFloat(secondNumber);
+    let result = operate(operadorFromOperation, firstNumber, secondNumber);
+    paraDisplay.textContent = result;
+});
+
